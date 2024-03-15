1d366debcedd53e4d7b3169bd8fe845a795bcba7
aissamenouissi <163555623+aissamenouissi@users.noreply.github.com>
2024-03-15 11:14:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Test GitHub')
-
+    print ('Test reussi!')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
